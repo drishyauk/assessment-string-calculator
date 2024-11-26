d7ae0e1ec65101e242d933b8b89ab3c59b4a407c
dhrishyaunni <dhrishya.unnikrishnan@ondosystems.com>
2024-11-26 14:38:39 +0530

    feat:created string calculator assessment

---
+++ src/pages/Calculator.tsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import "../index.css";
+
+function Calculator() {
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState<number | string | null>(null);
+
+  const handleChange = (e: any) => {
+    setInput(e.target.value);
+  };
+
+  const calculateResult = () => {
+    try {
+      if (input.trim() === "") {
+        setResult(0);
+      } else {
+        // Please add the delimiters here
+        const numbers = input.split(/[\n,\/\/;:=%]+/);
+
+        if (/[,|\n]$/.test(input)) {
+          throw new Error("Invalid input: Trailing comma or newline");
+        }
+
+        const negativeNumbers = numbers.filter((num) => Number(num) < 0);
+
+        if (negativeNumbers.length > 0) {
+          setResult(
+            `Negative numbers not allowed: ${negativeNumbers.join(", ")}`
+          );
+          return;
+        }
+        const sum = numbers.reduce((acc, curr) => acc + Number(curr), 0);
+        setResult(sum);
+      }
+    } catch (error) {
+      setResult("Error");
+    }
+  };
+
+  return (
+    <div className="calculator-container">
+      <h2>String Calculator</h2>
+      <textarea
+        value={input}
+        onChange={handleChange}
+        placeholder="Enter numbers separated by a custom delimiter or new lines/commas"
+        className="input-textarea"
+      />
+      <button onClick={calculateResult} className="calculate-button">
+        Calculate
+      </button>
+      {result !== null && (
+        <div
+          className={
+            typeof result === "string" ? "result error" : "result success"
+          }
+        >
+          <strong>Result:</strong> {result}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Calculator;
